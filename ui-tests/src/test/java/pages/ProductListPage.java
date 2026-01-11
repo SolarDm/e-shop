6194f80cc79a915a311673f6770a28e3773f4111
SolarDm <Dima0608806@gmail.com>
2026-01-11 18:29:15 +0300

    UI Tests

---
+++ ui-tests/src/test/java/pages/ProductListPage.java
@@ -0,0 +1,151 @@
+package pages;
+
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class ProductListPage {
+    private final SelenideElement pageHeader = $(".page-header");
+    private final SelenideElement pageTitle = pageHeader.$("h1");
+    private final SelenideElement productsCount = pageHeader.$("p");
+    private final SelenideElement searchInput = $(".search-input");
+    private final SelenideElement searchButton = $(".search-btn");
+    private final SelenideElement toggleFiltersButton = $(".toggle-filters-btn");
+    private final SelenideElement filtersPanel = $(".filters-panel");
+    private final SelenideElement clearFiltersButton = $(".clear-filters-btn");
+    
+    private final ElementsCollection productCards = $$(".product-card");
+    private final ElementsCollection categoryButtons = $$(".category-btn");
+    private final SelenideElement sortSelect = $(".sort-select");
+    private final ElementsCollection priceInputs = $$(".price-input");
+    private final SelenideElement minPriceInput = priceInputs.get(0);
+    private final SelenideElement maxPriceInput = priceInputs.get(1);
+    private final SelenideElement pagination = $(".pagination");
+    private final SelenideElement emptyProducts = $(".empty-products");
+    
+    public ProductListPage open() {
+        Selenide.open("/");
+        return this;
+    }
+    
+    public ProductListPage searchProduct(String productName) {
+        searchInput.setValue(productName);
+        searchButton.click();
+        waitForLoading();
+        return this;
+    }
+    
+    public ProductListPage toggleFilters() {
+        toggleFiltersButton.click();
+        return this;
+    }
+    
+    public ProductListPage clearAllFilters() {
+        clearFiltersButton.click();
+        waitForLoading();
+        return this;
+    }
+    
+    public ProductListPage selectCategory(String categoryName) {
+        categoryButtons.find(text(categoryName)).click();
+        waitForLoading();
+        return this;
+    }
+    
+    public ProductListPage sortBy(String sortOption) {
+        sortSelect.selectOption(sortOption);
+        waitForLoading();
+        return this;
+    }
+    
+    public ProductListPage setPriceRange(int min, int max) {
+        maxPriceInput.setValue(String.valueOf(max));
+        minPriceInput.setValue(String.valueOf(min));
+        waitForLoading();
+        return this;
+    }
+    
+    public ProductListPage addProductToCart(int productIndex) {
+        productCards.get(productIndex)
+            .$(".add-to-cart-btn")
+            .click();
+        return this;
+    }
+    
+    public ProductDetailPage openProductDetails(int productIndex) {
+        productCards.get(productIndex)
+            .$(".view-details-btn")
+            .click();
+        return new ProductDetailPage();
+    }
+    
+    public String getProductName(int productIndex) {
+        return productCards.get(productIndex)
+            .$(".product-title")
+            .getText();
+    }
+    
+    public String getProductPrice(int productIndex) {
+        return productCards.get(productIndex)
+            .$(".price-value")
+            .getText()
+            .replace(" ₽", "");
+    }
+    
+    public ProductListPage shouldHaveTitle(String expectedTitle) {
+        pageTitle.shouldHave(text(expectedTitle));
+        return this;
+    }
+    
+    public ProductListPage shouldHaveProductsCount(int expectedCount) {
+        productCards.shouldHave(CollectionCondition.size(expectedCount));
+        return this;
+    }
+    
+    public ProductListPage shouldHaveProductWithName(String productName) {
+        productCards.find(text(productName)).shouldBe(visible);
+        return this;
+    }
+    
+    public ProductListPage shouldHavePriceInRange(int minPrice, int maxPrice) {
+        for (SelenideElement card : productCards) {
+            String priceText = card.$(".price-value").getText();
+            int price = Integer.parseInt(priceText.replaceAll("[^0-9]", ""));
+            assert price >= minPrice && price <= maxPrice : 
+                String.format("Цена %d вне диапазона %d-%d", price, minPrice, maxPrice);
+        }
+        return this;
+    }
+    
+    public ProductListPage shouldShowEmptyState() {
+        emptyProducts.shouldBe(visible);
+        return this;
+    }
+    
+    public ProductListPage shouldShowFiltersPanel(boolean expectedVisible) {
+        if (expectedVisible) {
+            filtersPanel.shouldBe(visible);
+        } else {
+            filtersPanel.shouldNotBe(visible);
+        }
+        return this;
+    }
+    
+    public ProductListPage waitForLoading() {
+        $(".loading").shouldNotBe(visible);
+        return this;
+    }
+    
+    public int getTotalProductsFromHeader() {
+        String countText = productsCount.getText();
+        return Integer.parseInt(countText.replaceAll("[^0-9]", ""));
+    }
+    
+    public int getDisplayedProductsCount() {
+        return productCards.size();
+    }
+}
